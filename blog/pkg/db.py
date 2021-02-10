e12f6bf729670b4ce2ccf9b6bc933e5ffda8ac41
lim-mil <limyel@outlook.com>
2021-02-11 00:49:32 +0800

    去掉连接数据库

--- blog/pkg/db.py
+++ blog/pkg/db.py
@@ -12,6 +12,4 @@ def sqlite_connect():
     from blog.apps.project.model import ProjectModel, ProjectCategoryModel
     from blog.apps.user.models import UserModel
 
-
-    sqlite_db.connect()
     sqlite_db.create_tables([PostModel, PostCategoryModel, ProjectModel, ProjectCategoryModel, InfoModel, BlogrolModel, UserModel])
