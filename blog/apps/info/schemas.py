75f17e0f20f094a230364bdcb372cddb10e6969a
lim-mil <limyel@outlook.com>
2021-03-01 17:12:50 +0800

    info 默认值

--- blog/apps/info/schemas.py
+++ blog/apps/info/schemas.py
@@ -6,11 +6,11 @@ from blog.schemas import BaseSchema, DatetimeMixin, BaseResponse
 
 
 class BaseInfo(BaseSchema):
-    name: str = Field(..., max_length=64)
-    description: str = Field(..., max_length=256)
-    location: str = Field(..., max_length=64)
-    job: str = Field(..., max_length=64)
-    link: str = Field(..., max_length=512)
+    name: str = Field('limyel', ..., max_length=64)
+    description: str = Field('Pythonista', ..., max_length=256)
+    location: str = Field('福州', max_length=64)
+    job: str = Field('None', max_length=64)
+    link: str = Field('https://github.com/lim-mil', max_length=512)
 
 
 class BaseBlogrol(BaseSchema):
@@ -19,7 +19,7 @@ class BaseBlogrol(BaseSchema):
 
 
 class AboutInResponse(BaseSchema):
-    about: str
+    about: str = Field('')
 
 
 class InfoInUpdate(BaseSchema):
